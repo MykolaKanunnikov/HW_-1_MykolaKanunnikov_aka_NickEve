9e0e7ac711a68b25e863627b963691d8e5c82150
Nick Eve <kanunnikovmykola@gmail.com>
2021-12-15 23:22:40 +0200

    It's my first hw at the cursor. I wanted to make code free of redundancy as my current knowledge lets. I've suddenly found Arrays.toString --> seems that helped!

---
+++ src/education/cursor/Main.java
@@ -0,0 +1,28 @@
+package education.cursor;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int x, y, a, w, e, b;
+        x = 2;
+        y = 4;
+        a = 6;
+        w = 8;
+        e = 10;
+        b = 12;
+
+        int result1 = x + y - a / 2 * (x + w);
+        int result2 = a * (x + x) / y - a * 3;
+        int result3 = a + b / x - y;
+        int result4 = x - e / 2 % e + 1;
+
+        int[] resultArray = new int[]{result1, result2, result3, result4};
+
+        System.out.println(Arrays.toString(resultArray));
+
+
+    }
+}
