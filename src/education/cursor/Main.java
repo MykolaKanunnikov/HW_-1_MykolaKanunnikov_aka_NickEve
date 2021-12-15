82cdb3952b1b0acc166b19ab31ec33ea403fb1b3
Nick Eve <kanunnikovmykola@gmail.com>
2021-12-16 00:22:10 +0200

    Added 'X' variable as per task

--- src/education/cursor/Main.java
+++ src/education/cursor/Main.java
@@ -6,18 +6,19 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int x, y, a, w, e, b;
+        int x, y, a, w, e, b, X;
         x = 2;
         y = 4;
         a = 6;
         w = 8;
         e = 10;
         b = 12;
+        X = 14;
 
         int result1 = x + y - a / 2 * (x + w);
         int result2 = a * (x + x) / y - a * 3;
         int result3 = a + b / x - y;
-        int result4 = x - e / 2 % e + 1;
+        int result4 = X - e / 2 % e + 1;
 
         int[] resultArray = new int[]{result1, result2, result3, result4};
 
